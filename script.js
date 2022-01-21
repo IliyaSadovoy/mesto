ce7e487e381229748afa7ddb0ad5010135922f91
IliyaSadovoy <cain@yandex.ru>
2022-01-21 22:34:11 +0400

    Проект Место первая итерация

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
-function openPopup(){
+function openPopup() {
     popup.classList.add('popup_opened');
 }
 editButton.addEventListener('click', openPopup);
-function closePopup(){
+function closePopup() {
     popup.classList.remove('popup_opened')
 }
 closeButton.addEventListener('click', closePopup);
+let formElement = document.querySelector('.form')
+let nameInput = document.querySelector('.form__item_el_name')
+let professionInput = document.querySelector('.form__item_el_profession')
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    nameInput = nameInput.value;
+    professionInput = professionInput.value;
+    let profileName = document.querySelector('.profile__name');
+    let profileProfession = document.querySelector('.profile__profession')
+    profileName.textContent = nameInput;
+    profileProfession.textContent = professionInput;
+    popup.classList.remove('popup_opened')
+}
+formElement.addEventListener('submit', formSubmitHandler);
